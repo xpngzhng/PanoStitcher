7ebe534e7afa7b1c384d8e87b4fac422ad830d7c
zhengxuping <xupingzheng@gmail.com>
2016-04-14 11:21:53 +0800

    新增用带alpha通道图片添加水印logo的LogoFilter。

--- source/Task/PanoramaTaskUtil.cpp
+++ source/Task/PanoramaTaskUtil.cpp
@@ -1,4 +1,7 @@
 #include "PanoramaTaskUtil.h"
+#include "Image.h"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
 
 bool loadPhotoParams(const std::string& cameraParamFile, std::vector<PhotoParam>& params)
 {
@@ -133,4 +136,91 @@ bool prepareSrcVideos(const std::vector<std::string>& srcVideoFiles, bool bgr24,
     }
 
     return ok;
+}
+
+static void alphaBlend(cv::Mat& image, const cv::Mat& logo)
+{
+    CV_Assert(image.data && (image.type() == CV_8UC3 || image.type() == CV_8UC4) &&
+        logo.data && logo.type() == CV_8UC4 && image.size() == logo.size());
+
+    int rows = image.rows, cols = image.cols, channels = image.channels();
+    for (int i = 0; i < rows; i++)
+    {
+        unsigned char* ptrImage = image.ptr<unsigned char>(i);
+        const unsigned char* ptrLogo = logo.ptr<unsigned char>(i);
+        for (int j = 0; j < cols; j++)
+        {
+            if (ptrLogo[3])
+            {
+                int val = ptrLogo[3];
+                int comp = 255 - ptrLogo[3];
+                ptrImage[0] = (comp * ptrImage[0] + val * ptrLogo[0] + 254) / 255;
+                ptrImage[1] = (comp * ptrImage[1] + val * ptrLogo[1] + 254) / 255;
+                ptrImage[2] = (comp * ptrImage[2] + val * ptrLogo[2] + 254) / 255;
+            }
+            ptrImage += channels;
+            ptrLogo += 4;
+        }
+    }
+}
+
+bool LogoFilter::init(int width_, int height_, int type_)
+{
+    initSuccess = false;
+    if (width_ < 0 || height_ < 0 || (type_ != CV_8UC3 && type_ != CV_8UC4))
+        return false;
+
+    width = width_;
+    height = height_;
+    type = type_;
+
+    cv::Mat origLogo(logoHeight, logoWidth, CV_8UC4, logoData);
+    cv::imshow("logo", origLogo);
+    cv::waitKey(0);
+
+    int blockWidth = 300, blockHeight = 300;
+    rects.clear();
+    if (width < logoWidth || height < logoHeight)
+    {
+        cv::Rect logoRect(logoWidth / 2 - width / 2, logoHeight / 2 - height / 2, width, height);
+        logo = origLogo(logoRect);
+        rects.push_back(cv::Rect(0, 0, width, height));
+    }
+    else
+    {
+        logo = origLogo;
+        int w = (width + blockWidth - 1) / blockWidth, h = (height + blockHeight - 1) / blockHeight;
+        cv::Rect full(0, 0, width, height);
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                cv::Rect thisRect = cv::Rect(j * blockWidth + blockWidth / 2 - logoWidth / 2, 
+                                             i * blockHeight + blockHeight / 2 - logoHeight / 2, 
+                                             logoWidth, logoHeight) & 
+                                    full;
+                if (thisRect.area())
+                    rects.push_back(thisRect);
+            }
+        }
+    }
+
+    initSuccess = true;
+    return true;
+}
+
+bool LogoFilter::addLogo(cv::Mat& image)
+{
+    if (!initSuccess || !image.data || image.rows != height || image.cols != width || image.type() != type)
+        return false;
+
+    int size = rects.size();
+    for (int i = 0; i < size; i++)
+    {
+        cv::Mat imagePart(image, rects[i]);
+        cv::Mat logoPart(logo, cv::Rect(0, 0, rects[i].width, rects[i].height));
+        alphaBlend(imagePart, logoPart);
+    }
+
+    return true;
 }
--- source/Task/PanoramaTaskUtil.h
+++ source/Task/PanoramaTaskUtil.h
@@ -9,3 +9,15 @@ bool loadPhotoParams(const std::string& cameraParamFile, std::vector<PhotoParam>
 
 bool prepareSrcVideos(const std::vector<std::string>& srcVideoFiles, bool bgr24, const std::vector<int>& offsets,
     int tryAudioIndex, std::vector<avp::AudioVideoReader>& readers, int& audioIndex, cv::Size& srcSize, int& validFrameCount);
+
+struct LogoFilter
+{
+    LogoFilter() : initSuccess(false), width(0), height(0), type(0) {}
+    bool init(int width, int height, int type);
+    bool addLogo(cv::Mat& image);
+    
+    int width, height, type;
+    cv::Mat logo;
+    std::vector<cv::Rect> rects;
+    bool initSuccess;
+};
