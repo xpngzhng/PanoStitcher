84bb1004ca139666b1548d3fc27d3556c5015075
zhengxuping <xupingzheng@gmail.com>
2016-07-27 08:49:09 +0800

    删掉注释

--- source/IntelOpenCL/Pyramid.cpp
+++ source/IntelOpenCL/Pyramid.cpp
@@ -45,33 +45,6 @@ void pyramidDown8UC1To8UC1(const IOclMat& src, IOclMat& dst, cv::Size dstSize)
     cl_kernel kernel = iocl::pyrDown8UC1To8UC1->kernel;
     cl_command_queue queue = iocl::ocl->queue;
 
-    /*
-    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&src.mem);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 1, sizeof(int), &src.rows);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 2, sizeof(int), &src.cols);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 3, sizeof(int), (void *)&src.step);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 4, sizeof(cl_mem), (void *)&dst.mem);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 5, sizeof(int), &dst.rows);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 6, sizeof(int), &dst.cols);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 7, sizeof(int), &dst.step);
-    SAMPLE_CHECK_ERRORS(err);
-
-    size_t globalWorkSize[2] = { (size_t)round_up_aligned(src.cols, 256), (size_t)round_up_aligned(dst.rows, 1) };
-    size_t localWorkSize[2] = { 256, 1 };
-    size_t offset[2] = { 0, 0 };
-
-    err = clEnqueueNDRangeKernel(queue, kernel, 2, offset, globalWorkSize, localWorkSize, 0, NULL, NULL);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clFinish(queue);
-    SAMPLE_CHECK_ERRORS(err);
-    */
     PYR_DOWN_MAIN_BODY;
 }
 
@@ -92,33 +65,6 @@ void pyramidDown8UC4To8UC4(const IOclMat& src, IOclMat& dst, cv::Size dstSize)
     cl_kernel kernel = iocl::pyrDown8UC4To8UC4->kernel;
     cl_command_queue queue = iocl::ocl->queue;
 
-    /*
-    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&src.mem);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 1, sizeof(int), &src.rows);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 2, sizeof(int), &src.cols);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 3, sizeof(int), (void *)&src.step);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 4, sizeof(cl_mem), (void *)&dst.mem);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 5, sizeof(int), &dst.rows);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 6, sizeof(int), &dst.cols);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 7, sizeof(int), &dst.step);
-    SAMPLE_CHECK_ERRORS(err);
-
-    size_t globalWorkSize[2] = { (size_t)round_up_aligned(src.cols, 256), (size_t)round_up_aligned(dst.rows, 1) };
-    size_t localWorkSize[2] = { 256, 1 };
-    size_t offset[2] = { 0, 0 };
-
-    err = clEnqueueNDRangeKernel(queue, kernel, 2, offset, globalWorkSize, localWorkSize, 0, NULL, NULL);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clFinish(queue);
-    SAMPLE_CHECK_ERRORS(err);
-    */
     PYR_DOWN_MAIN_BODY;
 }
 
@@ -139,33 +85,6 @@ void pyramidDown8UC4To32SC4(const IOclMat& src, IOclMat& dst, cv::Size dstSize)
     cl_kernel kernel = iocl::pyrDown8UC4To32SC4->kernel;
     cl_command_queue queue = iocl::ocl->queue;
 
-    /*
-    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&src.mem);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 1, sizeof(int), &src.rows);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 2, sizeof(int), &src.cols);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 3, sizeof(int), (void *)&src.step);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 4, sizeof(cl_mem), (void *)&dst.mem);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 5, sizeof(int), &dst.rows);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 6, sizeof(int), &dst.cols);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 7, sizeof(int), &dst.step);
-    SAMPLE_CHECK_ERRORS(err);
-
-    size_t globalWorkSize[2] = { (size_t)round_up_aligned(src.cols, 256), (size_t)round_up_aligned(dst.rows, 1) };
-    size_t localWorkSize[2] = { 256, 1 };
-    size_t offset[2] = { 0, 0 };
-
-    err = clEnqueueNDRangeKernel(queue, kernel, 2, offset, globalWorkSize, localWorkSize, 0, NULL, NULL);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clFinish(queue);
-    SAMPLE_CHECK_ERRORS(err);
-    */
     PYR_DOWN_MAIN_BODY;
 }
 
@@ -186,33 +105,6 @@ void pyramidDown32FC1(const IOclMat& src, IOclMat& dst, cv::Size dstSize)
     cl_kernel kernel = iocl::pyrDown32FC1->kernel;
     cl_command_queue queue = iocl::ocl->queue;
 
-    /*
-    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&src.mem);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 1, sizeof(int), &src.rows);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 2, sizeof(int), &src.cols);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 3, sizeof(int), (void *)&src.step);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 4, sizeof(cl_mem), (void *)&dst.mem);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 5, sizeof(int), &dst.rows);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 6, sizeof(int), &dst.cols);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 7, sizeof(int), &dst.step);
-    SAMPLE_CHECK_ERRORS(err);
-
-    size_t globalWorkSize[2] = { (size_t)round_up_aligned(src.cols, 256), (size_t)round_up_aligned(dst.rows, 1) };
-    size_t localWorkSize[2] = { 256, 1 };
-    size_t offset[2] = { 0, 0 };
-
-    err = clEnqueueNDRangeKernel(queue, kernel, 2, offset, globalWorkSize, localWorkSize, 0, NULL, NULL);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clFinish(queue);
-    SAMPLE_CHECK_ERRORS(err);
-    */
     PYR_DOWN_MAIN_BODY;
 }
 
@@ -233,33 +125,6 @@ void pyramidDown32FC4(const IOclMat& src, IOclMat& dst, cv::Size dstSize)
     cl_kernel kernel = iocl::pyrDown32FC4->kernel;
     cl_command_queue queue = iocl::ocl->queue;
 
-    /*
-    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&src.mem);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 1, sizeof(int), &src.rows);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 2, sizeof(int), &src.cols);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 3, sizeof(int), (void *)&src.step);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 4, sizeof(cl_mem), (void *)&dst.mem);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 5, sizeof(int), &dst.rows);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 6, sizeof(int), &dst.cols);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clSetKernelArg(kernel, 7, sizeof(int), &dst.step);
-    SAMPLE_CHECK_ERRORS(err);
-
-    size_t globalWorkSize[2] = { (size_t)round_up_aligned(src.cols, 256), (size_t)round_up_aligned(dst.rows, 1) };
-    size_t localWorkSize[2] = { 256, 1 };
-    size_t offset[2] = { 0, 0 };
-
-    err = clEnqueueNDRangeKernel(queue, kernel, 2, offset, globalWorkSize, localWorkSize, 0, NULL, NULL);
-    SAMPLE_CHECK_ERRORS(err);
-    err = clFinish(queue);
-    SAMPLE_CHECK_ERRORS(err);
-    */
     PYR_DOWN_MAIN_BODY;
 }
 
